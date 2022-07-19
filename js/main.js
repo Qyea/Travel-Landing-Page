001430b5ab160db724d0fc4108ab1f5122626c50
Qyea <kreshess@mail.ru>
2022-07-19 20:41:29 +0300

    update: header, menu, tours, footer

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+      if (window.pageYOffset > 150){
+          header.classList.add('header_active');
+      }else{ header.classList.remove('header_active');
+      }  
+    };
+ }());
